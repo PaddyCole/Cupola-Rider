6fc65ba03dd1b043e6eec36ba4463ce4e406f0ed
Lars Modig <larswillymodig@gmail.com>
2013-04-21 11:42:10 +0200

    height aspect corrected as well

--- earth/ISS.js
+++ earth/ISS.js
@@ -2,6 +2,8 @@ google.load("earth", "1");
    
 var ge = null;
 
+var aspectRatio = 2000 / 1325;
+
 
 function init() {
 	google.earth.createInstance("map-canvas", initCallback, failureCallback);
@@ -62,15 +64,20 @@ function initScreenOverlay() {
 	// Set the overlay's size in pixels
 	screenOverlay.getSize().setXUnits(ge.UNITS_PIXELS);
 	screenOverlay.getSize().setYUnits(ge.UNITS_PIXELS);
-	screenOverlay.getSize().setX($(window).width());
-	screenOverlay.getSize().setY($(window).width()/1.509);
 
 	ge.getFeatures().appendChild(screenOverlay);
+
+	updateScreenOverlay();
 
 }
 
 function updateScreenOverlay() {
-	ge.getFeatures().getFirstChild().getSize().setX($(window).width());
-	ge.getFeatures().getFirstChild().getSize().setY($(window).width()/1.509);
+	if ( ($(window).width() / $(window).height()) > aspectRatio ) {
+		ge.getFeatures().getFirstChild().getSize().setX($(window).width());
+		ge.getFeatures().getFirstChild().getSize().setY($(window).width()/1.509);
+	} else {
+		ge.getFeatures().getFirstChild().getSize().setX($(window).height()/0.6255);
+		ge.getFeatures().getFirstChild().getSize().setY($(window).height());
+	}
 }
 
