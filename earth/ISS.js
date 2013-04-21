e2ff13bf6d923204d4fb846582bcc37b08c2af5b
Lars Modig <larswillymodig@gmail.com>
2013-04-21 13:30:41 +0200

    Realtime updating fixed

--- earth/ISS.js
+++ earth/ISS.js
@@ -4,6 +4,20 @@ var ge = null;
 
 var aspectRatio = 2000 / 1325;
 
+var fps = 10;
+var issPollInterval = 10000; //10 sec
+
+var lastTimestamp = 0;
+var currentTimestamp = 0;
+var timestampIteration = 1;
+
+var lastLatitude = 0;
+var lastLongitude = 0;
+var currentLatitude = 0;
+var currentLongitude = 0;
+var projectedLatitude = 0;
+var projectedLongitude = 0;
+
 
 function init() {
 	google.earth.createInstance("map-canvas", initCallback, failureCallback);
@@ -18,32 +32,68 @@ function init() {
 function initCallback(object) {
 	ge = object;
 	ge.getWindow().setVisibility(true);
-	//     INITIAL_CAMERA_ALTITUDE = 809273.5;
-	var camera = ge.getView().copyAsCamera(ge.ALTITUDE_RELATIVE_TO_GROUND);
-	camera.setTilt(camera.getTilt() + 43);
-	camera.setRoll(camera.getRoll() + -15);
 
-	initScreenOverlay();
+	//First call for cordinates, do some special stuff when we get them
+ 	$.getJSON('API.php', function(data) {
+ 		var camera = ge.getView().copyAsCamera(ge.ALTITUDE_RELATIVE_TO_GROUND);
+		camera.setTilt(camera.getTilt() + 43);
+		camera.setRoll(camera.getRoll() + -15);
+		camera.setLatitude(data.iss_position.latitude);
+		camera.setLongitude(data.iss_position.longitude);
+		camera.setAltitude(1809273.5);
+		ge.getView().setAbstractView(camera);
+		ge.getOptions().setFlyToSpeed(ge.SPEED_TELEPORT);
+		initScreenOverlay();
+	});
 
+	setInterval(function(){
+		timestampIteration = timestampIteration + 1;
+		if(lastTimestamp != 0) {
+			var timeDiff = currentTimestamp - lastTimestamp;
+			var timeIterations = timeDiff * fps;
+			projectedLatitude = currentLatitude 
+					+ ((currentLatitude - lastLatitude) 
+						* (timestampIteration / timeIterations)
+					);
+			projectedLongitude = currentLongitude
+					+ ((currentLongitude - lastLongitude) 
+						* (timestampIteration / timeIterations)
+					);
+			updateCamera(projectedLatitude,	projectedLongitude);
+		}
+	},1000/fps);
  
 	setInterval(function(){	
- 				var feed = "API.php";
- 				$.getJSON(feed, function(data) {
- 					var latitude = data.iss_position.latitude;
- 					var longitude = data.iss_position.longitude;
+		fetchISSLoc();
+	},issPollInterval);
 
- 					// Set new latitude and longitude values.
- 					camera.setLatitude(latitude);
- 					camera.setLongitude(longitude);
- 					camera.setAltitude(1809273.5);
-
- 					// Update the view in Google Earth.
- 					ge.getView().setAbstractView(camera);
- 				});
- 			},5000);
+	setTimeout(fetchISSLoc,2000);
+	setTimeout(fetchISSLoc,4000);
 }
 
 function failureCallback(object) {
+}
+
+function fetchISSLoc() {
+	$.getJSON('API.php', function(data) {
+		lastTimestamp = currentTimestamp;
+		lastLatitude = currentLatitude;
+		lastLongitude = currentLongitude;
+		currentTimestamp = data.timestamp;
+		currentLatitude = data.iss_position.latitude;
+		currentLongitude = data.iss_position.longitude;
+		timestampIteration = 0;
+	});
+}
+
+function updateCamera(latitude, longitude) {
+	var camera = ge.getView().copyAsCamera(ge.ALTITUDE_RELATIVE_TO_GROUND);
+
+	camera.setLatitude(latitude);
+	camera.setLongitude(longitude);
+	camera.setAltitude(1809273.5);
+
+	ge.getView().setAbstractView(camera);
 }
 
 function initScreenOverlay() {
@@ -55,13 +105,6 @@ function initScreenOverlay() {
 	icon.setHref(document.URL + 'iss-view-sharper.png');
 	screenOverlay.setIcon(icon);
 
-	// Set the ScreenOverlay's position in the window
-	//screenOverlay.getOverlayXY().setXUnits(ge.UNITS_PIXELS);
-	//screenOverlay.getOverlayXY().setYUnits(ge.UNITS_PIXELS);
-	//screenOverlay.getOverlayXY().setX(200);
-	//screenOverlay.getOverlayXY().setY(100);
-
-	// Set the overlay's size in pixels
 	screenOverlay.getSize().setXUnits(ge.UNITS_PIXELS);
 	screenOverlay.getSize().setYUnits(ge.UNITS_PIXELS);
 
